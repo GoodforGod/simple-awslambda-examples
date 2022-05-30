c96cecac74cfb83f74c37fcf979d52ab4c9dbdf3
Anton Kurako <goodforgod.dev@gmail.com>
2022-05-31 01:25:01 +0300

    [0.28.0-SNAPSHOT] Dependencies updated

--- simple-helloworld/build.sh
+++ simple-helloworld/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+gradlew shadowJar
 docker build -t simple-awslambda-helloworld .
 docker run --rm --entrypoint cat simple-awslambda-helloworld /home/application/function.zip > build/function.zip
