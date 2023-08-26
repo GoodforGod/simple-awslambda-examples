8d790fa86c7e70c1d45c72edf562fa0489f9f65e
Anton Kurako <goodforgod.dev@gmail.com>
2023-08-27 00:25:03 +0300

    [1.0.0-SNAPSHOT] Pre release 1.0.0 examples

--- simple-micronaut-helloworld/src/main/java/io/goodforgod/simplelambda/micronaut/LambdaHandler.java
+++ simple-micronaut-helloworld/src/main/java/io/goodforgod/simplelambda/micronaut/LambdaHandler.java
@@ -3,8 +3,10 @@ package io.goodforgod.simplelambda.micronaut;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import io.goodforgod.aws.lambda.simple.micronaut.MicronautInputLambdaEntrypoint;
+import io.goodforgod.graalvm.hint.annotation.InitializationHint;
 import io.goodforgod.graalvm.hint.annotation.NativeImageHint;
 import io.goodforgod.simplelambda.micronaut.aop.PrintMethod;
+import io.goodforgod.slf4j.simplelogger.SimpleLogger;
 import io.micronaut.core.annotation.Introspected;
 import jakarta.inject.Singleton;
 import java.util.UUID;
@@ -16,6 +18,7 @@ import org.slf4j.LoggerFactory;
  * @since 31.07.2021
  */
 @NativeImageHint(entrypoint = MicronautInputLambdaEntrypoint.class, name = "application")
+@InitializationHint(types = SimpleLogger.class)
 @Introspected
 @Singleton
 public class LambdaHandler implements RequestHandler<Request, Response> {
